f5bb9da10a239c8f46dcbe09dfb1cb880a5057c8
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-03-29 12:11:19 +0000

    createWebHistory updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home/Home.vue'
 
 const routes =
@@ -184,8 +184,7 @@ const routes =
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 })
-
 export default router
