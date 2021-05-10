eaf4a4bf353a78c2b83d21f23dc3fcc2621448d0
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-10 14:24:23 +0000

    Responsive done for home update 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,7 @@ module.exports =
 
         screens:
         {
-            'xs': {'min': '260px', 'max': '639px'},
+            'xs': {'min': '200px', 'max': '639px'},
             'sm': {'min': '640px', 'max': '767px'},
             'md': {'min': '768px', 'max': '1023px'},
             'lg': {'min': '1024px', 'max': '1279px'},
