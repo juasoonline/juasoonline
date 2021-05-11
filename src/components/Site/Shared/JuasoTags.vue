f4bc87eefa2c0b01e2e6a988d4ff19a69424c24f
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-11 23:02:08 +0000

    Home responsive (JuasoTags) 3 done

--- src/components/Site/Shared/JuasoTags.vue
+++ src/components/Site/Shared/JuasoTags.vue
@@ -70,11 +70,11 @@
 
             <div class="flex items-center bg-gray-100 rounded p-1 text-xxs">
                 <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                <p class="leading-tight">24/7 help center</p>
+                <p class="leading-tight">24/7 support center</p>
             </div>
             <div class="flex items-center bg-gray-100 rounded p-1 text-xxs">
                 <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
-                <p class="leading-tight">Secure payment</p>
+                <p class="leading-tight">Secure payment options</p>
             </div>
             <div class="flex items-center bg-gray-100 rounded p-1 text-xxs">
                 <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
