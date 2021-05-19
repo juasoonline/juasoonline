abf31b00b5c4524ea92a0d7f051b09ebedbaf640
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-19 14:09:54 +0000

    Responsive for Account page update 1

--- src/components/Dashboard/Account/MainContents.vue
+++ src/components/Dashboard/Account/MainContents.vue
@@ -127,7 +127,7 @@
             <div class="px-2 mx-auto">
 
                 <!-- Begin user actions -->
-                <div class="bg-white rounded -mt-10 flex justify-between px-3">
+                <div class="bg-white rounded -mt-10 flex justify-between py-2">
                     <div class="mx-auto text-xxs py-2">
                         <router-link to="wishlist">
                             <svg class="w-5 h-5 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
