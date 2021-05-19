4adea3bb7d7e1a9cdb63960191382da82ef543c4
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-19 15:16:45 +0000

    Responsive for Account page update 3

--- src/components/Dashboard/Account/MainContents.vue
+++ src/components/Dashboard/Account/MainContents.vue
@@ -147,25 +147,25 @@
                 <div class="mt-3">
                     <div class="bg-white rounded">
                         <div class="border-b">
-                            <router-link to="orders" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="orders" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
                                 <span>My Orders</span>
                             </router-link>
                         </div>
                         <div class="border-b">
-                            <router-link to="orders" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="orders" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
                                 <span>My Favourite Store</span>
                             </router-link>
                         </div>
                         <div class="border-b">
-                            <router-link to="orders" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="orders" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
                                 <span>Message Center</span>
                             </router-link>
                         </div>
                         <div class="">
-                            <router-link to="orders" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="orders" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                                 <span>Setting</span>
                             </router-link>
@@ -174,30 +174,30 @@
 
                     <div class="bg-white rounded mt-3">
                         <div class="border-b">
-                            <div to="orders" class="flex text-xs items-center p-3 cursor-pointer">
+                            <div to="orders" class="flex text-sm items-center p-3 cursor-pointer">
                                 <span class="font-bold text-sm">My Account</span>
                             </div>
                         </div>
                         <div class="border-b">
-                            <router-link to="" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                                 <span>Edit Account</span>
                             </router-link>
                         </div>
                         <div class="border-b">
-                            <router-link to="" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
                                 <span>Change Password</span>
                             </router-link>
                         </div>
                         <div class="border-b">
-                            <router-link to="" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                                 <span>Change Email</span>
                             </router-link>
                         </div>
                         <div class="">
-                            <router-link to="" class="flex text-xs items-center p-3 cursor-pointer">
+                            <router-link to="" class="flex text-sm items-center p-3 cursor-pointer">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                                 <span>Change Address</span>
                             </router-link>
