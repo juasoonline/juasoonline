b29fac513da04a7918d896d70ceae6b321ca7183
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-19 09:25:12 +0000

    Responsive for Login, Registration, ForgotPassword done

--- src/components/Dashboard/ForgotPassword/MainContents.vue
+++ src/components/Dashboard/ForgotPassword/MainContents.vue
@@ -1,15 +1,23 @@
 <template>
 
     <!-- Begin contents -->
-    <main class="container mx-auto my-4">
-        <section class="my-40">
-                <div class="w-1/4 m-auto justify-center">
+    <main class="2xl:container xl:container lg:container md:w-screen sm:w-screen xs:w-screen mx-auto my-4">
+        <section class="2xl:my-40 xl:my-40 lg:my-40 md:my-20 sm:my-20 xs:my-20">
+            <div class="2xl:w-1/4 xl:w-1/4 lg:w-1/4 m-auto justify-center">
+                <div class="md:px-1.5 sm:px-1.5 xs:px-1.5">
+
+                    <!-- Begin logo -->
+                    <div class="">
+                        <img :src="files.logo" class="2xl:w-32 xl:w-32 lg:w-32 md:w-24 sm:w-24 xs:w-24 mx-auto">
+                    </div>
+                    <!-- End logo -->
 
                     <!-- Begin forgot password form -->
                     <forgot-password-form></forgot-password-form>
                     <!-- End forgot password form -->
 
                 </div>
+            </div>
         </section>
     </main>
     <!-- End contents -->
@@ -18,11 +26,17 @@
 
 <script>
     import ForgotPasswordForm from "./ForgotPasswordForm";
+    import { reactive } from "vue";
 
     export default
     {
         name: "MainContents",
         components: { ForgotPasswordForm },
+        setup()
+        {
+            const files = reactive ({ userIconMale: '../assets/images/user-icon-male.png', logo: '../assets/images/logo.png' })
+            return { files }
+        }
     }
 </script>
 
--- src/components/Dashboard/Register/MainContents.vue
+++ src/components/Dashboard/Register/MainContents.vue
@@ -2,13 +2,13 @@
 
     <!-- Begin contents -->
     <main class="2xl:container xl:container lg:container md:w-screen sm:w-screen xs:w-screen mx-auto my-4">
-        <section class="my-40">
+        <section class="2xl:my-40 xl:my-40 lg:my-40 md:my-20 sm:my-20 xs:my-20">
             <div class="2xl:w-1/4 xl:w-1/4 lg:w-1/4 m-auto justify-center">
                 <div class="md:px-1.5 sm:px-1.5 xs:px-1.5">
 
                     <!-- Begin logo -->
                     <div class="">
-                        <img :src="files.logo" class="2xl:w-28 xl:w-28 lg:w-28 md:w-20 sm:w-20 xs:w-20 mx-auto">
+                        <img :src="files.logo" class="2xl:w-32 xl:w-32 lg:w-32 md:w-24 sm:w-24 xs:w-24 mx-auto">
                     </div>
                     <!-- End logo -->
 
