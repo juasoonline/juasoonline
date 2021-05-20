ee397a464705df67b1d208b5cd4cd2bc22c5fd46
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-20 11:55:00 +0000

    Responsive for Product page update 6 (TopAd removed)

--- src/components/Site/Shared/TopAds.vue
+++ src/components/Site/Shared/TopAds.vue
@@ -11,12 +11,12 @@
             <!-- Begin ad banner -->
 
             <!-- Begin sm banner -->
-            <div class="2xl:hidden xl:hidden lg:hidden md:block sm:block xs:block py-2">
-                <router-link to="/" class="uppercase text-xxs px-2 py-1 bg-gradient-to-r from-red-400 to-red-500 text-gray-100 rounded">
-                    <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path></svg>
-                    <span class="ml-1">Open App</span>
-                </router-link>
-            </div>
+<!--            <div class="2xl:hidden xl:hidden lg:hidden md:block sm:block xs:block py-2">-->
+<!--                <router-link to="/" class="uppercase text-xxs px-2 py-1 bg-gradient-to-r from-red-400 to-red-500 text-gray-100 rounded">-->
+<!--                    <svg class="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path></svg>-->
+<!--                    <span class="ml-1">Open App</span>-->
+<!--                </router-link>-->
+<!--            </div>-->
             <!-- Begin sm banner -->
 
         </div>
