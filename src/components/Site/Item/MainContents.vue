962fd276c054e84c80ba2e9c0740b8fd12c113ee
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-30 01:32:47 +0000

    Item page mobile view updated (Store area added)

--- src/components/Site/Item/MainContents.vue
+++ src/components/Site/Item/MainContents.vue
@@ -384,8 +384,8 @@
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
                     </div>
                     <p class="text-sm mt-1">{{ product.item.name }}</p>
-                    <span class="block text-gray-500 text-xxs flex items-center mt-2">
-                        <svg class="w-3 h-3 text-red-600 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
+                    <span class="block text-gray-500 text-xs flex items-center mt-2">
+                        <svg class="w-3.5 h-3.5 text-red-600 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
                         4.9 | {{ product.item.total_sold }} Sold
                     </span>
                 </div>
@@ -536,15 +536,46 @@
             </div>
             <!-- End overview, review and specification info -->
 
-            <!-- Begin seller recommendations -->
+            <!-- Begin seller and recommendations -->
             <div class="bg-white px-2 py-3 mb-3">
 
-                <!-- Begin header -->
+                <!-- Begin store name -->
+                <div class="flex items-center">
+                    <svg class="w-4 h-4 text-gray-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"></path></svg>
+                    <span class="text-sm font-bold"><router-link :to="{ name: 'Store', params: { store: product.store.resource_id }}">{{ product.store.name }}</router-link></span>
+                </div>
+                <!-- End store name -->
+
+                <!-- Begin store stats -->
+                <div class="flex items-center justify-between my-3">
+                    <div class="text-left">
+                        <p class="text-xxs">95.7%</p>
+                        <p class="text-gray-400 text-xxxs">Positive Feedback</p>
+                    </div>
+                    <div class="text-center">
+                        <p class="text-xxs">230</p>
+                        <p class="text-gray-400 text-xxxs">Items</p>
+                    </div>
+                    <div class="text-right">
+                        <p class="text-xxs">230</p>
+                        <p class="text-gray-400 text-xxxs">Followers</p>
+                    </div>
+                </div>
+                <!-- End store stats -->
+
+                <!-- Begin call to action -->
+                <div class="my-3 border-b pb-4">
+                    <router-link :to="{ name: 'Store', params: { store: product.store.resource_id }}" class="bg-red-600 text-white mr-3 text-xxs py-0.5 px-3.5 border rounded-full border-red-600">Go to store</router-link>
+                    <button class="text-red-600 text-xxs py-0.5 px-3.5 border rounded-full border-red-600">Follow</button>
+                </div>
+                <!-- End call to action -->
+
+                <!-- Begin recommendations header -->
                 <div class="flex justify-between items-center mb-3">
                     <h3 class="font-bold text-xs mb-1.5">Seller Recommendations</h3>
                     <p class="text-xxxs text-juaso-secondary"><router-link :to="{ name: 'Store', params: { store: product.store.resource_id }}">View All Product</router-link></p>
                 </div>
-                <!-- End header -->
+                <!-- End recommendations header -->
 
                 <!-- Begin items list -->
                 <div class="grid 2xl:gap-4 xl:gap-4 lg:gap-4 md:gap-2 sm:gap-2 xs:gap-1 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-3">
@@ -562,7 +593,7 @@
                 <!-- End items list -->
 
             </div>
-            <!-- End seller recommendations -->
+            <!-- End seller and recommendations -->
 
             <!-- Begin more to love -->
             <div class="px-2 mb-16">
