0024d34f5c52e98ed077ac688d6f0d4f98a13962
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-30 01:50:36 +0000

    Item page mobile view (Store area added) update 2

--- src/components/Site/Item/MainContents.vue
+++ src/components/Site/Item/MainContents.vue
@@ -380,10 +380,10 @@
             <div class="bg-white p-2">
                 <div class="border-b pb-2.5">
                     <div class="flex items-center justify-between">
-                        <span class="font-bold text-lg">GHS {{ product.item.sales_price }}</span>
-                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                        <span class="font-bold text-xl">GHS {{ product.item.sales_price }}</span>
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
                     </div>
-                    <p class="text-sm mt-1">{{ product.item.name }}</p>
+                    <p class="text-sm mt-2">{{ product.item.name }}</p>
                     <span class="block text-gray-500 text-xs flex items-center mt-2">
                         <svg class="w-3.5 h-3.5 text-red-600 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
                         4.9 | {{ product.item.total_sold }} Sold
@@ -572,8 +572,8 @@
 
                 <!-- Begin recommendations header -->
                 <div class="flex justify-between items-center mb-3">
-                    <h3 class="font-bold text-xs mb-1.5">Seller Recommendations</h3>
-                    <p class="text-xxxs text-juaso-secondary"><router-link :to="{ name: 'Store', params: { store: product.store.resource_id }}">View All Product</router-link></p>
+                    <h3 class="font-bold text-sm mb-1.5">Seller Recommendations</h3>
+                    <p class="text-xxs text-juaso-secondary"><router-link :to="{ name: 'Store', params: { store: product.store.resource_id }}">View All Product</router-link></p>
                 </div>
                 <!-- End recommendations header -->
 
@@ -584,7 +584,7 @@
                             <img v-bind:src="item.image" :alt="item.name" class="object-cover h-16 w-16 text-center mx-auto">
                         </router-link>
                         <div class="m-3 text-center">
-                            <p class="font-bold block text-xxxs my-0.5">
+                            <p class="font-bold block text-xxs my-0.5">
                                 <router-link class="w-full object-cover hover:text-red-500" :to="{ name: 'Item', params: { item: item.resource_id }}">GHS {{ item.sales_price }}</router-link>
                             </p>
                         </div>
