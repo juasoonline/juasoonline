d772cb3c5703404a0f6b4c5bf6005ee1195bb9fa
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-05-30 12:46:06 +0000

    Item page mobile view (Store area added) update 5

--- src/components/Site/Item/MainContents.vue
+++ src/components/Site/Item/MainContents.vue
@@ -380,9 +380,9 @@
             <div class="bg-white p-2">
                 <div class="border-b pb-2.5">
                     <div class="flex items-center justify-between">
-                        <p class="font-bold text-lg items-center">
+                        <p class="font-bold text-lg items-center flex">
                             GHS {{ product.item.sales_price }}
-                            <del class="text-xxs text-red-600 font-light mr-1">GHS {{ product.item.price }}</del>
+                            <del class="text-xxs text-red-600 font-light mx-2">GHS {{ product.item.price }}</del>
                             <span class="inline-flex py-0.5 px-3 items-center rounded text-xxxs bg-red-200 text-red-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">- {{ product.item.percentage_charge }}%</span>
                         </p>
                         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
