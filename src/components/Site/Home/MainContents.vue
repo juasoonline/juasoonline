bb0676371e679b034660a68e7bfc725446fac290
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-06-10 00:49:52 +0000

    Homepage: infinit scroll for items done

--- src/components/Site/Home/MainContents.vue
+++ src/components/Site/Home/MainContents.vue
@@ -36,27 +36,7 @@
             <!-- End title -->
 
             <!-- Begin items list -->
-            <div class="grid 2xl:gap-4 xl:gap-4 lg:gap-4 md:gap-2 sm:gap-2 xs:gap-1 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2">
-                <div v-for="item in product.items" :key="item.attributes.resource_id" class="card bg-white rounded overflow-hidden shadow-md hover:shadow-2xl">
-                    <router-link class="w-full object-cover" :to="{ name: 'Item', params: { item: item.attributes.resource_id }}">
-                        <img class="object-cover h-48 w-full" v-bind:src="item.attributes.image" :alt="item.include.images[0].attributes.image">
-                    </router-link>
-                    <div class="m-2">
-                        <span class="text-gray-500 text-xs hover:text-red-500 leading-tight">
-                            <router-link class="w-full object-cover" :to="{ name: 'Item', params: { item: item.attributes.resource_id } }">
-                                <p class="leading-5" :title="item.attributes.name">{{ item.attributes.name.substring(0, 25) }}...</p>
-                            </router-link>
-                        </span>
-                        <p class="font-bold block text-xs my-0.5">
-                            <router-link class="w-full object-cover text-gray-700 hover:text-red-500" :to="{ name: 'Item', params: { item: item.attributes.resource_id } }">
-                                GHS {{ item.attributes.sales_price }}
-                                <del class="ml-2 text-xxxs font-light text-gray-500 text-red-500">GHS {{ item.attributes.price }}</del>
-                            </router-link>
-                        </p>
-                        <span class="block text-gray-500 text-xxs">{{ item.attributes.total_sold }} Sold</span>
-                    </div>
-                </div>
-            </div>
+            <items-list></items-list>
             <!-- End items list -->
 
             <!-- Begin load more button -->
@@ -77,33 +57,13 @@
     import CatsAndAdSlides from "@/components/Site/Home/CatsAndAdSlides";
     import JuasoTags from "@/components/Site/Shared/JuasoTags";
     import QuickDeals from "./QuickDeals";
-
-    import { onBeforeMount, onErrorCaptured, reactive, ref } from 'vue';
-    import axios from "axios";
+    import ItemsList from "./ItemsList";
 
     export default
     {
         name: "MainContents",
-        components: { CatsAndAdSlides, JuasoTags, QuickDeals },
-        setup ()
-        {
-            const loader = reactive({ color: '#686868', size: '25px', loading: true, isLoading: false, position: 'center' })
-            const product = reactive({ items: [] })
-            const error = ref(null )
+        components: { CatsAndAdSlides, JuasoTags, QuickDeals, ItemsList },
 
-            onErrorCaptured( e => {
-                error.value = e;
-            })
-
-            onBeforeMount(() =>
-            {
-                axios({ method: 'GET', url: 'juaso/products?include=images', headers: {} })
-                    .then( response => { product.items = response.data.data; } )
-                    .catch( error => { error.response })
-            })
-
-            return { product, error, loader }
-        }
     }
 </script>
 
