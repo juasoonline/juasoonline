4e79579dd65c36737842cd97e7361b1d003916ee
Michael <billyston@gmail.com>
2021-11-01 22:43:37 +0000

    Task: Products in group, Desc: Get product based on a given group, category or subcategory, Status: Testing

--- src/components/Site/Category/gp/MainContents.vue
+++ src/components/Site/Category/gp/MainContents.vue
@@ -76,6 +76,58 @@
                 <!-- End sort and view type -->
 
                 <!-- Begin items list -->
+                <div v-if="product.loaded === true">
+                    <div v-if="product.items.length > 0" id="infinite-list" class="grid gap-4 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-3 xs:grid-cols-2">
+                        <div v-for="( item ) in product.items" :key="item.attributes.resource_id" class="card bg-white rounded overflow-hidden shadow-md hover:shadow-2xl">
+                            <router-link class="w-full object-cover" :to="{ name: 'Item', params: { item: item.attributes.resource_id }}">
+                                <img class="object-cover h-48 w-full" v-bind:src="item.attributes.image" :alt="item.attributes.image">
+                            </router-link>
+                            <div class="m-2">
+                                <p class="text-gray-500 text-xs hover:text-red-500 leading-tight">
+                                    <router-link class="w-full object-cover" :to="{ name: 'Item', params: { item: item.attributes.resource_id } }">
+                                        <span class="leading-5" :title="item.attributes.name">{{ item.attributes.name.substring(0, 20) }}...</span>
+                                    </router-link>
+                                </p>
+                                <p v-if="item.pricing.priced === 'Product'" class="font-bold block text-xs my-0.5">
+                                    <router-link class="w-full object-cover text-gray-700 hover:text-red-500" :to="{ name: 'Item', params: { item: item.attributes.resource_id } }">
+                                        {{ item.pricing.price_data[0].sales_price }}
+                                        <del class="ml-2 text-xxs font-light text-gray-500 text-red-500"> {{ item.pricing.price_data[0].price }}</del>
+                                    </router-link>
+                                </p>
+                                <p v-else class="font-bold block text-xs my-0.5">
+                                    <router-link class="w-full object-cover text-gray-700 hover:text-red-500" :to="{ name: 'Item', params: { item: item.attributes.resource_id } }">
+                                        {{ item.pricing.price_data[0].price_range }}
+                                    </router-link>
+                                </p>
+                                <p class="text-gray-500 text-xs hover:text-red-500 leading-tight my-1.5">
+                                    <router-link class="w-full object-cover" :to="{ name: 'Item', params: { item: item.attributes.resource_id } }">
+                                        <span class="leading-5" :title="item.store.name">{{ item.store.name }}</span>
+                                    </router-link>
+                                </p>
+                                <div class="flex items-center justify-between">
+                                    <p class="block text-xs my-0.5 flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1 text-red-500" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+                                        </svg>
+                                        <span>{{ item.attributes.average_rating }}</span>
+                                    </p>
+                                    <router-link :to="{ name: 'Item', params: { item: item.attributes.resource_id }}" class="hover:text-red-500">
+                                        <span class="text-xs font-light text-gray-500">{{ item.attributes.total_sold }} Sold</span>
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-else class="h-96">
+                        <p class="py-2 text-sm text-gray-400">There are no products in this category</p>
+                    </div>
+                </div>
+                <div v-else>
+                    <img class="mx-auto text-center w-20 h-20" src="https://juasoonline.nyc3.digitaloceanspaces.com/assets/images/loader.gif" alt="">
+                </div>
+                <div v-if="isLoading">
+                    <img class="mx-auto text-center w-20 h-20" src="https://juasoonline.nyc3.digitaloceanspaces.com/assets/images/loader.gif" alt="">
+                </div>
                 <!-- End items list -->
 
             </div>
@@ -142,7 +194,7 @@
                 currentPage.value++
                 try
                 {
-                    const response = await axios({ method: 'GET', url: 'juaso/categories/' + route.params.category + '/products' })
+                    const response = await axios({ method: 'GET', url: 'juaso/groups/' + route.params.category + '/products' })
                     const parsedResponse = await response.data
                     product.items = [ ...product.items, ...parsedResponse.data ]
                     totalPages.value = parsedResponse.meta.last_page
