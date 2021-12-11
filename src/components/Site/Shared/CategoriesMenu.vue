91f258a0130bcababf1fe483dec99ec8b6734e79
Michael <billyston@gmail.com>
2021-12-11 10:32:22 +0000

    Task: Category Menu, Desc: Category menu modified, Status: Modified

--- src/components/Site/Shared/CategoriesMenu.vue
+++ src/components/Site/Shared/CategoriesMenu.vue
@@ -4,11 +4,11 @@
     <div class="w-full">
 
         <!-- Begin cat header -->
-        <div class="bg-juaso-primary rounded-t">
+        <div class="bg-gray-50 text-gray-700 font-bold border-b border-gray-100 rounded-t">
           <router-link to="/categories">
-              <div class="flex justify-start p-2.5 hover:bg-juaso-secondary rounded-t">
-                  <svg class="w-6 h-6 mr-1.5 text-gray-50" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"></path></svg>
-                  <span class="text-gray-50">Categories</span>
+              <div class="flex justify-start items-center p-2.5 hover:bg-gray-200 rounded-t">
+                  <svg class="w-5 h-5 mr-1.5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"></path></svg>
+                  <span class="">Categories</span>
               </div>
           </router-link>
         </div>
@@ -21,13 +21,14 @@
                     <li v-for="group in groups.categories" :key="group.attributes.resource_id" class="2xl:text-ct xl:text-xs lg:text-xs text-gray-500">
 
                         <!-- Begin group menu link -->
-                        <router-link :to="{ name: 'Group', params: { category: group.attributes.resource_id, slug: group.attributes.slug }}" class="flex justify-between items-center py-1.5 2xl:mb-0.5 px-3 hover:shadow-lg hover:text-juaso-primary">
+                        <router-link :to="{ name: 'Group', params: { category: group.attributes.resource_id, slug: group.attributes.slug }}" class="flex justify-between items-center py-1.5 2xl:mb-0.5 px-3 hover:shadow-lg hover:text-gray-700">
                             <span>{{ group.attributes.name }}</span>
                             <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
                         </router-link>
                         <!-- End group menu link -->
 
-                        <div class="megadrop rounded-r border shadow-sm grid gap-6 grid-cols-3">
+                        <!-- Begin categories and subcategories -->
+                        <div class="megadrop rounded-r border border-gray-100 shadow-sm grid gap-6 grid-cols-3">
                           <div class="col" v-for="category in group.include.categories" :key="category.attributes.resource_id">
 
                               <!-- Begin category menu link -->
@@ -51,6 +52,8 @@
                               </ul>
                           </div>
                         </div>
+                        <!-- Begin categories and subcategories -->
+
                     </li>
                 </ul>
             </nav>
